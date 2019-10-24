ab582f8229b9d0e471920c10e58db33f60bec45f
brunomktd <bruno.mktd@gmail.com>
2019-10-23 22:04:47 -0300

    correção de bug

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,9 @@ import User from './app/models/User';
 const routes = new Router();
 
 routes.get('/', async (req, res) => {
-  console.log('iniciando a chamada');
   const user = await User.create({
     name: 'Bruno de Oliveira',
-    email: 'novoemail@gmail.com.br',
+    email: 'bruno.strax@gmail.com.br',
     password_hash: '123456',
   });
 
