4699bc22bea6367329786d461c06991b179fde85
Bruno de Oliveira <bruno.mktd@gmail.com>
2019-11-04 16:07:01 -0300

    Criação de fila de e-mail com nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,19 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'b0cb498adbd45b',
+    pass: '4158382cd855b9',
+  },
+  default: {
+    from: 'Team Go Barber <noreply@gobarber.com>',
+  },
+};
+
+// Amazon SES
+// Mailgun
+// Sparkpost
+// Mandril (Mailchimp)
+// Gmail
+// Mailtrap (dev)
